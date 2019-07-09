ad86a031ba0c4510d805d312ef661d8e204472c2
enter1024 <946726585@qq.com>
2019-07-10 01:42:32 +0800

    计算属性

--- js/vueInstance.js
+++ js/vueInstance.js
@@ -5,7 +5,9 @@ var v = new Vue({
 	data: {
 		// 实例对象可以直接访问的数据
 		// 建议将要使用到的数据预先在data内声明
-		message: '通过插值语法{{message}}就可以访问数据'
+		message: '通过插值语法{{message}}就可以访问数据',
+		country: "中国",
+		address: "广东省-深圳市-福田区-下沙社区-65坊605"
 	},
 	
 	// 里面的方法可以直接被调用，通常使用<@事件>的方式调用
@@ -15,10 +17,18 @@ var v = new Vue({
 		}
 	},
 	
-	//计算属性(注重将结果返回)，计算属性有缓存
+	/**
+	 * 1.计算属性(注重将结果返回)，计算属性有缓存
+	 * 2.计算属性里面定义的函数名可以直接在html代码中使用，通过{{sum}}即可以显示结果
+	 * 3.计算属性里面的方法无需调用，所依赖的数据一旦改变，自动执行并返回或显示最新结果
+	 * 4.如果一个功能可以使用computed，watch，methods等方式实现，优先使用computed
+	 */
 	computed: {
 		sum: function(num1,num2){
 			return num1+num2;
+		},
+		getFullAddress: function() {
+			return this.country + this.address; // 依赖的数据一旦改变自动执行该方法
 		}
 	},
 	
